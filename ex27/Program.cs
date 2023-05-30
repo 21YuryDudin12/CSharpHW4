e9c2d642437a6102737458d4b47b8dd34ef00f0b
Yury Dudin <cepob33@gmail.com>
2023-05-31 01:03:46 +0500

    ex 27 complite

---
+++ ex27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите число. Программа посчитает сумму цифр в нем");
+int digit1 = int.Parse(Console.ReadLine());
+void FindDigitsSum (int digit)
+{
+int res = 0;
+while (digit > 0)
+{
+    res = digit % 10 + res;
+    digit = digit / 10;
+}
+Console.WriteLine(res);
+}
+FindDigitsSum(digit1);
