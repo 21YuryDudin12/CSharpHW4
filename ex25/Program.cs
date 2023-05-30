02cf592eb21f2123383cb3974680e0c5b1a5136c
Yury Dudin <cepob33@gmail.com>
2023-05-31 00:32:29 +0500

    ex25 complite

---
+++ ex25/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Введите число а, которое нужно возвести в степень, затем введите степень, в которую нужно возвести число");
+int qa = int.Parse(Console.ReadLine());
+int qb = int.Parse(Console.ReadLine());
+int FindDigitSt (int a, int b)
+{
+int res = a;
+int i = 0;
+for (i = 2; i <= b; i++)
+{
+res = res *a;
+}
+return res;
+}
+FindDigitSt(qa, qb);
+Console.WriteLine(FindDigitSt(qa, qb));
+
