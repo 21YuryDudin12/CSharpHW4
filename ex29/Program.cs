4ecb017ded7c0c3541f8ede7761d803d83b5b5df
Yury Dudin <cepob33@gmail.com>
2023-05-31 00:54:48 +0500

    ex 29 complite

---
+++ ex29/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Программа задает массив из 8 элементов и выводит их на экран");
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    int a = new Random().Next(1, 100);
+    Console.WriteLine(a);
+}
